ea481fd9bad1fbb067349a65dddb5c5bd7b5c1ee
SatoshiTerasaki <s.terasaki@atelier-arith.jp>
2021-10-15 19:24:29 +0900

    fix typo

--- routes.jl
+++ routes.jl
@@ -5,6 +5,6 @@ using ParticlesController
 
 route("/") do
     maxT = min(100, parse(Int, params(:maxT, "50")))
-    num_particles = min(30 parse(Int, params(:num_particles, "10")))
+    num_particles = min(30, parse(Int, params(:num_particles, "10")))
     ParticlesController.rendergif(;maxT, num_particles)
 end
