392f58296a20eddbec5bca288e106cd1b23db1dd
SatoshiTerasaki <s.terasaki@atelier-arith.jp>
2021-10-15 16:34:39 +0900

    fix bug

--- routes.jl
+++ routes.jl
@@ -4,7 +4,7 @@ using Plots
 using ParticlesController
 
 route("/") do
-    maxT = min(1000, parse(Int, params(:maxT, 100)))
-    num_particles = min(100, parse(Int, params(:num_particles, 30)))
+    maxT = min(1000, parse(Int, params(:maxT, "100")))
+    num_particles = min(80, parse(Int, params(:num_particles, "30")))
     ParticlesController.rendergif(;maxT, num_particles)
 end
